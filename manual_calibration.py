968bee490895911ae396c0115a4974b0f6719180
Mika <isabellamika@gmail.com>
2023-01-31 12:40:19 +0900

    correct words

--- manual_calibration.py
+++ manual_calibration.py
@@ -16,8 +16,8 @@ from googletrans import Translator, constants
 HEIGHT = 768
 WIDTH = 1024
 
-POSITION = [-20, -370, -140]
-ROTATION = [-0.3, -0.5, 2.1]
+POSITION = [260, -234, -131]
+ROTATION = [0.1, 0.1, 0.1]
 ZOOM = [1, 1]
 DISTORTION = [0, 0, 0, 0, 0, 0]
 
@@ -45,6 +45,7 @@ def set_externals(input_cam, output_window_name):
     cam = cv.VideoCapture(input_cam)
     cam.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
     cam.set(cv.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+    #cam.set(cv.CAP_PROP_FPS, 1)
     ret, frame = cam.read()
     output = np.zeros([HEIGHT,WIDTH,3], dtype=np.uint8)
     output.fill(255)
@@ -115,46 +116,61 @@ def manual_calib(k):
     print("Distortion: " + str(DISTORTION))
 
 
-def ocr(frame, detector, custom_config):
+def ocr(frame, detector, translator, mecab, custom_config):
+    frame[:, :, 2] = np.zeros([frame.shape[0], frame.shape[1]])
     frame = get_grayscale(frame)
     frame = thresholding(frame)
     frame = remove_noise(frame)
     frame = opening(frame)
     frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
-    sentence = pytesseract.image_to_string(frame, config=custom_config, output_type=Output.DICT)["text"]
-    print(sentence)
-    sentence = sentence.strip().split("\n")
-    furigana = []
-    for s in sentence:
-        furigana = get_furigana(furigana, s)
+    #sentence = pytesseract.image_to_string(frame, config=custom_config, output_type=Output.DICT)["text"]
+    #print(sentence)
 
+    #sentence = sentence.strip().split("\n")
+
+    sentence = []
     results = pytesseract.image_to_data(frame, config=custom_config, output_type=Output.DICT)
-    print(results)
+    for i in range(0, len(results["text"])):
+        if len(sentence) <= results["line_num"][i]:
+            sentence.append(results["text"][i])
+        else:
+            sentence[results["line_num"][i]] += results["text"][i]
+
+    furigana = []
+    for posis, s in enumerate(sentence):
+        furigana = get_furigana(mecab, furigana, posis, s)
+
+    #print(results)
+    flag_first = 1
     for posif, f in enumerate(furigana):
         for i in range(0, len(results["text"])):
             conf = int(results["conf"][i])
             if conf > 70:
-                print("im detected", end=" ")
-                print(results["text"][i])
-                if any(is_kanji(_) for _ in results["text"][i]):
-                    print("im kanji", end = " ")
-                    print(results["text"][i])
-                    print(furigana)
-                    if results["text"][i] in f[0]:
-                        print("i have furigana", end=" ")
-                        print(results["text"][i])
-                        word_x = results["left"][i]
-                        word_y = results["top"][i]
+                #print("im detected", end=" ")
+                #print(results["text"][i])
+                if results["line_num"][i] == f[2]:
+                    if any(is_kanji(_) for _ in results["text"][i]):
+                        #print("im kanji", end = " ")
+                        #print(results["text"][i])
+                        #print(furigana)
+                        if results["text"][i] in f[0]:
+                            #print("i have furigana", end=" ")
+                            #print(results["text"][i])
+                            word_x = results["left"][i]
+                            word_y = results["top"][i]
 
-                        text = f[1]
-                        detector = write_japanese(detector, text, word_x, word_y)
-                        frame = write_japanese(frame, text, word_x, word_y)
-                        furigana[posif] = ["", ""]
-                        break
+                            if flag_first:
+                                detector.fill(200)
+                                flag_first = 0
+                            text = f[1]
+                            detector = write_japanese(detector, text, word_x, word_y)
+                            frame = write_japanese(frame, text, word_x, word_y)
+                            furigana[posif] = ["", ""]
+                            break
     return frame, detector
 
 
-def get_furigana(furigana, sentence):
+def get_furigana(mecab, furigana, posi, sentence):
     node = mecab.parseToNode(sentence)
     while node is not None:
         origin = node.surface
@@ -168,7 +184,7 @@ def get_furigana(furigana, sentence):
             else:
                 kana = node.surface
             hiragana = jaconv.kata2hira(kana)
-            furigana.append([origin, hiragana])
+            furigana.append([origin, hiragana, posi])
         node = node.next
     return furigana
 
@@ -178,11 +194,11 @@ def is_kanji(ch):
 
 
 def write_japanese(image, text, word_x, word_y):
-    font = ImageFont.truetype("Koruri-Regular.ttf", 14)
+    font = ImageFont.truetype("Koruri-Regular.ttf", 32)
 
     img_pil = Image.fromarray(image)
     draw = ImageDraw.Draw(img_pil)
-    draw.text((word_x + 20, word_y - 20), text, font=font, fill=(0, 0, 255, 0), stroke_width=1)
+    draw.text((word_x, word_y - 30), text, font=font, fill=(0, 0, 255, 0), stroke_width=1)
     image = np.array(img_pil)
     return image
 
@@ -192,63 +208,51 @@ def get_grayscale(image):
     return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 
-# noise removal
 def remove_noise(image):
     return cv.medianBlur(image, 3)
 
 
-# thresholding
 def thresholding(image):
-    return cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+    return cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 5)
 
 
-# dilation
 def dilate(image):
     kernel = np.ones((3, 3), np.uint8)
     return cv.dilate(image, kernel, iterations=1)
 
 
-# erosion
 def erode(image):
     kernel = np.ones((3, 3), np.uint8)
     return cv.erode(image, kernel, iterations=1)
 
 
-# opening - erosion followed by dilation
 def opening(image):
     kernel = np.ones((3, 3), np.uint8)
     return cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
 
 
-cam, output_window, detector = set_externals(0, "out")
-map_x, map_y = set_camera_configuration()
-mecab, translator, custom_config = set_text_detector()
-
-while True:
-    ret, frame = cam.read()
-    frame = cv.flip(frame, 1)
-
-    # just call this if manually positioning camera
-    #map_x, map_y = set_camera_configuration()
-    detector.fill(255)
-    frame = cv.remap(frame, map_x, map_y, interpolation=cv.INTER_LINEAR)
-    frame, detector = ocr(frame, detector, custom_config)
-
-
-
-
-    cv.imshow("check", frame)
-    cv.imshow("out", detector)
-
-    k = cv.waitKey(1000) & 0xFF
-    if k == ord('q'):
-        break
-
-    #manual_calib(k)
-
-
 def main():
-    return
+    cam, output_window, detector = set_externals(0, "out")
+    map_x, map_y = set_camera_configuration()
+    mecab, translator, custom_config = set_text_detector()
+
+    while True:
+        ret, frame = cam.read()
+        frame = cv.flip(frame, 1)
+
+        # just call this if manually positioning camera
+        #map_x, map_y = set_camera_configuration()
+        frame = cv.remap(frame, map_x, map_y, interpolation=cv.INTER_LINEAR)
+        frame, detector = ocr(frame, detector, translator, mecab, custom_config)
+
+        cv.imshow("check", frame)
+        cv.imshow("out", detector)
+
+        k = cv.waitKey(1000) & 0xFF
+        if k == ord('q'):
+            break
+
+        #manual_calib(k)
 
 
 if __name__ == '__main__':
